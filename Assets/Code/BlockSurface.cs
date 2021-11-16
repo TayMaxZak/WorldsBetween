8a6226d712cb6ca0de58c326aad2845ab7f2b8c7
Thomas Zakh <thomas.zakh@gmail.com>
2021-11-16 10:03:22 -0800

    Update BlockSurface.cs

--- Assets/Code/BlockSurface.cs
+++ Assets/Code/BlockSurface.cs
@@ -6,4 +6,10 @@ public class BlockSurface
 {
 	public Vector3 normal;
 	public Vector3 relativeOffset; // Where is the center of this surface relative to its associated block
+
+	public BlockSurface(Vector3 normal, Vector3 relativeOffset)
+	{
+		this.normal = normal;
+		this.relativeOffset = relativeOffset;
+	}
 }
