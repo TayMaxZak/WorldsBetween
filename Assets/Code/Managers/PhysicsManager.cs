edf8864766280a7bc99f51ecd716b16ebb0938fd
Thomas Zakh <tommyzakh@gmail.com>
2021-12-29 17:24:37 -0800

    Started work on physics managers

---
+++ Assets/Code/Managers/PhysicsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using System.Threading.Tasks;
+
+public partial class PhysicsManager : MonoBehaviour
+{
+	public static PhysicsManager Instance;
+
+	private List<Actor> actors;
+
+	private void Update()
+	{
+		
+	}
+}
