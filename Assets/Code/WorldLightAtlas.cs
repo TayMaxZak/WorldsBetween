edd0c18981b95c1c0ee7f20958ca0fd5b98e66a7
Thomas Zakh <thomas.zakh@gmail.com>
2022-01-19 22:31:20 -0800

    Added ambient lightmap

--- Assets/Code/WorldLightAtlas.cs
+++ Assets/Code/WorldLightAtlas.cs
@@ -16,7 +16,10 @@ public class WorldLightAtlas : MonoBehaviour
 	public static WorldLightAtlas Instance;
 
 	public Texture3D defaultLightmap;
-	public Texture3D liveLightmap;
+	public Texture3D defaultLightmap2;
+
+	public Texture3D directLightmap;
+	public Texture3D ambientLightmap;
 
 	private int size = 256;
 
@@ -28,14 +31,15 @@ public class WorldLightAtlas : MonoBehaviour
 	{
 		if (!Application.isPlaying)
 		{
-			ApplyTexture(defaultLightmap);
+			ApplyTexture(defaultLightmap, defaultLightmap2);
 		}
 		else
 		{
 			size = World.GetChunkSize() * (1 + World.Generator.GetGenRange() * 2);
 
-			CreateLightmap();
-			ApplyTexture(liveLightmap);
+			CreateDirectLightmap();
+			CreateAmbientLightmap();
+			ApplyTexture(directLightmap, ambientLightmap);
 
 			Instance = this;
 		}
@@ -43,27 +47,29 @@ public class WorldLightAtlas : MonoBehaviour
 
 	private void OnDestroy()
 	{
-		if (liveLightmap)
+		if (directLightmap)
 		{
 			Debug.Log("Destroyed");
 		}
 
-		ApplyTexture(defaultLightmap);
+		ApplyTexture(defaultLightmap, defaultLightmap2);
 	}
 
-	private void ApplyTexture(Texture texture)
+	private void ApplyTexture(Texture texture, Texture texture2)
 	{
 		Shader.SetGlobalTexture("LightMap", texture);
+		Shader.SetGlobalTexture("LightMap2", texture2);
 		Shader.SetGlobalFloat("LightMapScale", texture.width);
+		Shader.SetGlobalFloat("LightMapScale2", texture2.width);
 	}
 
-	private void CreateLightmap()
+	private void CreateDirectLightmap()
 	{
 		// Create the texture and apply the configuration
-		liveLightmap = new Texture3D(size, size, size, TextureFormat.RGBAHalf, false);
+		directLightmap = new Texture3D(size, size, size, TextureFormat.RGBAHalf, false);
 
-		liveLightmap.wrapMode = TextureWrapMode.Repeat;
-		liveLightmap.filterMode = FilterMode.Bilinear;
+		directLightmap.wrapMode = TextureWrapMode.Repeat;
+		directLightmap.filterMode = FilterMode.Bilinear;
 
 		Color[] colors = new Color[size * size * size];
 		for (int z = 0; z < size; z++)
@@ -74,23 +80,53 @@ public class WorldLightAtlas : MonoBehaviour
 				int yOffset = y * size;
 				for (int x = 0; x < size; x++)
 				{
-					RandomizeColor();
-
 					colors[x + yOffset + zOffset] = Color.black;
 				}
 			}
 		}
 
 		// Copy the color values to the texture
-		liveLightmap.SetPixels(colors);
+		directLightmap.SetPixels(colors);
 
 		// Apply the changes to the texture and upload the updated texture to the GPU
-		liveLightmap.Apply();
+		directLightmap.Apply();
+	}
+
+	private void CreateAmbientLightmap()
+	{
+		// One pixel per chunk
+		int size = this.size / 16;
+
+		// Create the texture and apply the configuration
+		ambientLightmap = new Texture3D(size, size, size, TextureFormat.RGBAHalf, false);
+
+		ambientLightmap.wrapMode = TextureWrapMode.Repeat;
+		ambientLightmap.filterMode = FilterMode.Bilinear;
+
+		Color[] colors = new Color[size * size * size];
+		for (int z = 0; z < size; z++)
+		{
+			int zOffset = z * size * size;
+			for (int y = 0; y < size; y++)
+			{
+				int yOffset = y * size;
+				for (int x = 0; x < size; x++)
+				{
+					colors[x + yOffset + zOffset] = Color.black;
+				}
+			}
+		}
+
+		// Copy the color values to the texture
+		ambientLightmap.SetPixels(colors);
+
+		// Apply the changes to the texture and upload the updated texture to the GPU
+		ambientLightmap.Apply();
 	}
 
 	public void WriteToLightmap(LightMapSpace texSpace, Vector3Int pos, Color value)
 	{
-		if (liveLightmap == null)
+		if (directLightmap == null)
 			return;
 
 		if (texSpace == LightMapSpace.WorldSpace)
@@ -99,7 +135,15 @@ public class WorldLightAtlas : MonoBehaviour
 		if (pos.x < 0 || pos.y < 0 || pos.z < 0 || pos.x >= size || pos.y >= size || pos.z >= size)
 			return;
 
-		liveLightmap.SetPixel(pos.x, pos.y, pos.z, value);
+		Color oldValue = directLightmap.GetPixel(pos.x, pos.y, pos.z);
+		directLightmap.SetPixel(pos.x, pos.y, pos.z, value);
+
+		int chunkSize = World.GetChunkSize();
+		float changeFraction = 1f / (chunkSize);
+
+		Color oldAmbValue = ambientLightmap.GetPixel(pos.x, pos.y, pos.z);
+		Color newAmbValue = oldAmbValue + (value - oldValue) * changeFraction;
+		ambientLightmap.SetPixel(pos.x / chunkSize, pos.y / chunkSize, pos.z / chunkSize, newAmbValue);
 
 		changeCount++;
 	}
@@ -110,7 +154,10 @@ public class WorldLightAtlas : MonoBehaviour
 			return;
 
 		if (changeCount > 0)
-			liveLightmap.Apply();
+		{
+			directLightmap.Apply();
+			ambientLightmap.Apply();
+		}
 		changeCount = 0;
 	}
 
