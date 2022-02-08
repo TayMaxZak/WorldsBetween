93f84e2eb5fe01bef5f13b5fc0c04d749d6753ae
Taylor <taylor.max.zak@gmail.com>
2022-02-08 13:58:07 -0800

    Improved menu animations, cleaned up camera shift script, added initial zoom

--- Assets/Code/UI/UIShiftCamera.cs
+++ Assets/Code/UI/UIShiftCamera.cs
@@ -4,31 +4,54 @@ using UnityEngine;
 
 public class UIShiftCamera : MonoBehaviour
 {
+	[Header("XY Movement")]
 	[SerializeField]
-	private Vector2 maxExtent = new Vector2(1,1);
-
+	private Vector2 xyMaxExtents = new Vector2(1,1);
 	[SerializeField]
-	private float damping = 1f;
+	private float xyDamping = 8f;
+	private bool mouseInWindow = false;
 
-	private float zPos;
+	[Header("Z Movement")]
+	[SerializeField]
+	private float zoomDamping = 2f;
+	[SerializeField]
+	private float zPosInitialZoom = -10f;
 
-	private Vector3 newPos;
+	/* Targets for lerping */
+	private float zPosTarget = 0;
+
+	private Vector3 xyPosTarget = Vector3.zero;
 
 	private void Awake()
 	{
-		zPos = transform.localPosition.z;
+		zPosTarget = transform.localPosition.z;
+
+		// Start lerping camera from inital z thats further away
+		transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, zPosInitialZoom);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		// Only start lerping once the cursor is in a normal position
+		if (2 * Mathf.Abs(Input.mousePosition.x / Screen.width - 0.5f) < 1 || 2 * Mathf.Abs(Input.mousePosition.y / Screen.height - 0.5f) < 1)
+			mouseInWindow = true;
+		if (!mouseInWindow)
+			return;
+
 		Vector2 mousePos = new Vector2(
-			maxExtent.x * Mathf.Clamp(2 * (Input.mousePosition.x / Screen.width - 0.5f), -1, 1),
-			maxExtent.y * Mathf.Clamp(2 * (Input.mousePosition.y / Screen.height - 0.5f), -1, 1)
+			xyMaxExtents.x * Mathf.Clamp(2 * (Input.mousePosition.x / Screen.width - 0.5f), -1, 1),
+			xyMaxExtents.y * Mathf.Clamp(2 * (Input.mousePosition.y / Screen.height - 0.5f), -1, 1)
 		);
 
-		newPos = new Vector3(mousePos.x, mousePos.y, zPos);
+		xyPosTarget = new Vector3(mousePos.x, mousePos.y, zPosTarget);
 
-		transform.localPosition = Vector3.Lerp(transform.localPosition, newPos, Time.deltaTime * damping);
+		// Main lerping
+		Vector3 newPos = Vector3.Lerp(transform.localPosition, xyPosTarget, Time.deltaTime * xyDamping);
+
+		// Zoom lerping
+		newPos.z = Mathf.Lerp(transform.localPosition.z, xyPosTarget.z, Time.deltaTime * zoomDamping);
+
+		transform.localPosition = newPos;
 	}
 }
