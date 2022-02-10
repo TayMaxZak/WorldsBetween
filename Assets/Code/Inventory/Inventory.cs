c8da16060921c9a3fdb878b12a633dbe366553c7
Taylor <taylor.max.zak@gmail.com>
2022-02-10 13:58:20 -0800

    Polished inventory layout

--- Assets/Code/Inventory/Inventory.cs
+++ Assets/Code/Inventory/Inventory.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 [System.Serializable]
 public class Inventory
 {
-	public Vector2Int dimensions = new Vector2Int(1,1);
+	public Vector2Int backpackSize = new Vector2Int(1, 1);
+	public InventorySlot[,] backpackSlots;
+
+	public InventorySlot holsterL = new InventorySlot() { heldItem = null, size = 2 };
+	public InventorySlot holsterR = new InventorySlot() { heldItem = null, size = 2 };
 
 	public struct InventorySlot
 	{
 		public Item heldItem;
+		public int size;
 	}
 }
